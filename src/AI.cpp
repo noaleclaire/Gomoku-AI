d191b6a6eee79df50e2c82761c15ef0f5217b97f
Laurent Enzo <enzo.laurent@epitech.eu>
2022-11-16 12:06:14 +0100

    [EDIT] : IA worked

--- src/AI.cpp
+++ src/AI.cpp
@@ -15,11 +15,9 @@ void AI::turn(Board &board)
     // std::size_t x = 0, y = 0;
     std::size_t x = std::rand()%DEFAULT_BOARD_SIZE, y = std::rand()%DEFAULT_BOARD_SIZE;
 
-    Printer::print("DEBUG ", "IA: x=", x, ", y=", y);
     while (!board.setPos(Board::CellState::SECOND_PLAYER, x, y)) {
-        x = std::rand()%DEFAULT_BOARD_SIZE - 1;
-        y = std::rand()%DEFAULT_BOARD_SIZE - 1;
-        Printer::print("DEBUG ", "Boucle IA: x=", x, ", y=", y);
+        x = std::rand()%DEFAULT_BOARD_SIZE;
+        y = std::rand()%DEFAULT_BOARD_SIZE;
     }
     Printer::print(x, ",", y);
 }
