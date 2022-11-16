be63a241a0cdb6939e21b698c6660ad854a21d7c
Laurent Enzo <enzo.laurent@epitech.eu>
2022-11-16 11:58:04 +0100

    [ADD] : debug in ia move

--- src/AI.cpp
+++ src/AI.cpp
@@ -6,6 +6,7 @@
 */
 
 #include "../include/AI.hpp"
+#include "../include/BrainCommand.hpp"
 #include "../include/define.hpp"
 #include "../include/Printer.hpp"
 
@@ -14,9 +15,11 @@ void AI::turn(Board &board)
     // std::size_t x = 0, y = 0;
     std::size_t x = std::rand()%DEFAULT_BOARD_SIZE - 1, y = std::rand()%DEFAULT_BOARD_SIZE - 1;
 
+    Printer::print("DEBUG ", "IA: x=", x, ", y=", y);
     while (!board.setPos(Board::CellState::SECOND_PLAYER, x, y)) {
         x = std::rand()%DEFAULT_BOARD_SIZE - 1;
         y = std::rand()%DEFAULT_BOARD_SIZE - 1;
+        Printer::print("DEBUG ", "Boucle IA: x=", x, ", y=", y);
     }
     Printer::print(x, ",", y);
 }
