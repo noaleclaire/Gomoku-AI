2638b53efaf880e37ab18a5226fb2e90da5ee14f
Laurent Enzo <enzo.laurent@epitech.eu>
2022-11-26 11:12:14 +0100

    [EDIT] : clean code

--- src/AI.cpp
+++ src/AI.cpp
@@ -25,13 +25,7 @@ void AI::turn(Board &board, std::size_t playerX, std::size_t playerY)
     } else if (nbFieldCell.first == 1 && nbFieldCell.second == 0) {
         _start(board, x, y, playerX, playerY);
     } else {
-        // if (_attack(board, x, y) == false) {
-        //     if (_defend(board, x, y) == false) {
-        //         _turn(board, x, y);
-        //     }
-        // }
-        if (yesy(board, x, y) == false)
-            _turn(board, x, y);
+        _turn(board, x, y);
     }
     board.setPos(Board::CellState::SECOND_PLAYER, x, y);
     // board.printBoard();
@@ -59,71 +53,7 @@ void AI::_start(Board &board, std::size_t &x, std::size_t &y, std::size_t player
     }
 }
 
-bool AI::_attack(Board &board, std::size_t &x, std::size_t &y)
-{
-    std::vector<Board::CellAttribute> line = {};
-    std::vector<Board::Direction> directions = {Board::Direction::HORIZONTAL, Board::Direction::VERTICAL,
-                                                Board::Direction::LEFTTORIGHT, Board::Direction::RIGHTTOLEFT};
-
-    for (std::size_t i = 0; i < board.getBoard().size(); i++) {
-        for (std::size_t j = 0; j < board.getBoard().at(i).size(); j++) {
-            for (auto &dir : directions) {
-                line = board.getLineWithMidCell(dir, j, i);
-                if (line.size() < 5)
-                    continue;
-                for (std::size_t lineIndex = 0; line.size() - lineIndex > 5; lineIndex++) {
-                    for (auto &pattern : Board::attackPattern) {
-                        for (std::size_t ptnIndex = 0; ptnIndex < 5; ptnIndex++) {
-                            if (line.at(lineIndex + ptnIndex).field != pattern.at(ptnIndex))
-                                break;
-                            if (line.at(lineIndex + ptnIndex).field == Board::CellState::EMPTY) {
-                                x = line.at(lineIndex + ptnIndex).posX;
-                                y = line.at(lineIndex + ptnIndex).posY;
-                            }
-                            if (ptnIndex == 4)
-                                return (true);
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return (false);
-}
-
-bool AI::_defend(Board &board, std::size_t &x, std::size_t &y)
-{
-    std::vector<Board::CellAttribute> line = {};
-    std::vector<Board::Direction> directions = {Board::Direction::HORIZONTAL, Board::Direction::VERTICAL,
-                                                Board::Direction::LEFTTORIGHT, Board::Direction::RIGHTTOLEFT};
-
-    for (std::size_t i = 0; i < board.getBoard().size(); i++) {
-        for (std::size_t j = 0; j < board.getBoard().at(i).size(); j++) {
-            for (auto &dir : directions) {
-                line = board.getLineWithMidCell(dir, j, i);
-                if (line.size() < 5)
-                    continue;
-                for (std::size_t lineIndex = 0; line.size() - lineIndex > 5; lineIndex++) {
-                    for (auto &pattern : Board::defensePattern) {
-                        for (std::size_t ptnIndex = 0; ptnIndex < 5; ptnIndex++) {
-                            if (line.at(lineIndex + ptnIndex).field != pattern.at(ptnIndex))
-                                break;
-                            if (line.at(lineIndex + ptnIndex).field == Board::CellState::EMPTY) {
-                                x = line.at(lineIndex + ptnIndex).posX;
-                                y = line.at(lineIndex + ptnIndex).posY;
-                            }
-                            if (ptnIndex == 4)
-                                return (true);
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return (false);
-}
-
-bool AI::yesy(Board &board, std::size_t &x, std::size_t &y)
+void AI::_turn(Board &board, std::size_t &x, std::size_t &y)
 {
     bool def = false;
     std::size_t defX = 0, defY = 0;
@@ -138,34 +68,10 @@ bool AI::yesy(Board &board, std::size_t &x, std::size_t &y)
                 if (line.size() < 5)
                     continue;
                 for (std::size_t lineIndex = 0; line.size() - lineIndex > 5; lineIndex++) {
-                    for (auto &pattern : Board::attackPattern) {
-                        for (std::size_t ptnIndex = 0; ptnIndex < 5; ptnIndex++) {
-                            if (line.at(lineIndex + ptnIndex).field != pattern.at(ptnIndex))
-                                break;
-                            if (line.at(lineIndex + ptnIndex).field == Board::CellState::EMPTY) {
-                                x = line.at(lineIndex + ptnIndex).posX;
-                                y = line.at(lineIndex + ptnIndex).posY;
-                            }
-                            if (ptnIndex == 4)
-                                return (true);
-                        }
-                    }
-                    for (auto &pattern : Board::defensePattern) {
-                        for (std::size_t ptnIndex = 0; ptnIndex < 5; ptnIndex++) {
-                            std::size_t emptyX = 0, emptyY = 0;
-                            if (line.at(lineIndex + ptnIndex).field != pattern.at(ptnIndex))
-                                break;
-                            if (line.at(lineIndex + ptnIndex).field == Board::CellState::EMPTY) {
-                                emptyX = line.at(lineIndex + ptnIndex).posX;
-                                emptyY = line.at(lineIndex + ptnIndex).posY;
-                            }
-                            if (ptnIndex == 4 && def == false) {
-                                defX = emptyX;
-                                defY = emptyY;
-                                def = true;
-                            }
-                        }
-                    }
+                    if (_attack(x, y, line, lineIndex) == true)
+                        return;
+                    if (def == false)
+                        _defend(defX, defY, line, lineIndex);
                 }
             }
         }
@@ -173,18 +79,51 @@ bool AI::yesy(Board &board, std::size_t &x, std::size_t &y)
     if (def == true) {
         x = defX;
         y = defY;
+    } else {
+        _exploration(board, x, y);
     }
-    return (def);
 }
 
-void AI::_turn(Board &board, std::size_t &x, std::size_t &y)
+bool AI::_attack(std::size_t &x, std::size_t &y, std::vector<Board::CellAttribute> line, std::size_t lineIndex)
 {
-    // x = std::rand()%DEFAULT_BOARD_SIZE;
-    // y = std::rand()%DEFAULT_BOARD_SIZE;
-    // while (!board.setPos(Board::CellState::SECOND_PLAYER, x, y)) {
-    //     x = std::rand()%DEFAULT_BOARD_SIZE;
-    //     y = std::rand()%DEFAULT_BOARD_SIZE;
-    // }
+    for (auto &pattern : Board::attackPattern) {
+        for (std::size_t ptnIndex = 0; ptnIndex < 5; ptnIndex++) {
+            if (line.at(lineIndex + ptnIndex).field != pattern.at(ptnIndex))
+                break;
+            if (line.at(lineIndex + ptnIndex).field == Board::CellState::EMPTY) {
+                x = line.at(lineIndex + ptnIndex).posX;
+                y = line.at(lineIndex + ptnIndex).posY;
+            }
+            if (ptnIndex == 4)
+                return (true);
+        }
+    }
+    return (false);
+}
+
+bool AI::_defend(std::size_t &defX, std::size_t &defY, std::vector<Board::CellAttribute> line, std::size_t lineIndex)
+{
+    for (auto &pattern : Board::defensePattern) {
+        for (std::size_t ptnIndex = 0; ptnIndex < 5; ptnIndex++) {
+            std::size_t emptyX = 0, emptyY = 0;
+            if (line.at(lineIndex + ptnIndex).field != pattern.at(ptnIndex))
+                break;
+            if (line.at(lineIndex + ptnIndex).field == Board::CellState::EMPTY) {
+                emptyX = line.at(lineIndex + ptnIndex).posX;
+                emptyY = line.at(lineIndex + ptnIndex).posY;
+            }
+            if (ptnIndex == 4) {
+                defX = emptyX;
+                defY = emptyY;
+                return (true);
+            }
+        }
+    }
+    return (false);
+}
+
+void AI::_exploration(Board &board, std::size_t &x, std::size_t &y)
+{
     int score = -9999;
 
     // Exploration a un de profondeur
